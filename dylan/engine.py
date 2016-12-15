393995d0606ad7501d2ed59ec8bb775f4584b0f4
kgregerson <kylegregerson@aggiemail.usu.edu>
2016-12-15 16:10:33 -0700

    New Engine

--- dylan/engine.py
+++ dylan/engine.py
@@ -1,6 +1,7 @@
 import abc
 import numpy as np
-from scipy.stats import binom
+from scipy.stats import binom, norm
+from math import sqrt, exp, log
 
 
 class PricingEngine(object, metaclass=abc.ABCMeta):
@@ -177,53 +178,147 @@ def LookbackOptionPricer(pricing_engine, option, data):
     
     return price
     
+class ControlVariateEngine(PricingEngine):
+    def __init__(self, replications, steps, alpha, Vbar, xi, pricer):
+        self.__replications = replications
+        self.__steps = steps
+        self.__pricer = pricer
+        self.__alpha = alpha
+        self.__Vbar = Vbar
+        self.__xi = xi
+        
+    @property
+    def replications(self):
+        return self.__replications
+    
+    @replications.setter
+    def replications(self, new_replications):
+        self.__replications = new_replications
+        
+    @property
+    def steps(self):
+        return self.__steps
+    
+    @steps.setter
+    def steps(self, new_steps):
+        self.__steps = new_steps
+    
+    @property
+    def alpha(self):
+        return self.__alpha
+    
+    @alpha.setter
+    def alpha(self, new_alpha):
+        self.__alpha = new_alpha
+    
+    @property
+    def Vbar(self):
+        return self.__Vbar
+    
+    @Vbar.setter
+    def Vbar(self, new_Vbar):
+        self.__Vbar = new_Vbar
+        
+    @property
+    def xi(self):
+        return self.__xi
+    
+    @xi.setter
+    def xi(self, new_xi):
+        self.__xi = new_xi
+    
+    def calculate(self, option, data):
+        return self.__pricer(self, option, data)
 
-def BlackScholesDelta(St, t, K, T, sig, r, div):
-    tau = T - t
-    d1 = (np.log(St/K) + (r - div + 0.5 * sig * sig) * tau) / (sig * np.sqrt(tau))
-    delta = np.exp(-div * tau) * norm.cfd(d1)
+def BlackScholesDelta(spot, t, strike, expiry, volatility, rate, dividend):
+    tau = expiry - t
+    d1 = (np.log(spot/strike) + (rate - dividend + 0.5 * volatility * volatility) * tau) / (volatility * np.sqrt(tau))
+    delta = np.exp(-dividend * tau) * norm.cdf(d1)
 
     return delta
+    
+def BlackScholesGamma(spot, t, strike, expiry, volatility, rate, dividend):
+    tau = expiry - t
+    d1 = (np.log(spot/strike) + (rate - dividend + 0.5 * volatility * volatility) * tau) / (volatility * np.sqrt(tau))
+    gamma = np.exp(-dividend * tau ) * (norm.pdf(d1) / spot * volatility * np.sqrt(tau))
+    return gamma
+    
+def BlackScholesVega(spot, t, strike, expiry, volatility, rate, dividend):
+    tau = expiry - t
+    d2 = (np.log(spot / strike) + (rate - dividend - volatility * volatility * 0.5) * tau)/( volatility * np.sqrt(tau))
+    vega = strike * exp(-rate * tau) * norm.pdf(d2) * np.sqrt(tau)
+    return vega
 
-
-def ControlVariatePricer(engine, option, data):
-    S = 100
-    K = 100
-    sig = 0.20
-    r = 0.06
-    T = 1.0
-    div = 0.03
-    N = 10
-    M = 100
+    
+def ControlVariateMCPricer(engine, option, data):
     beta1 = -1
-    dt = T / N
-    nudt = (r - div - 0.5 * sig**2) * dt
-    sigsdt = sig * sqrt(dt)
-    erddt = exp((r - div) * dt)
-
+    beta2 = -1
+    beta3 = -1
+    expiry = option.expiry
+    strike = option.strike
+    (spot, rate, volatility, dividend) = data.get_data()
+    steps = engine.steps
+    replications = engine.replications
+    alpha = engine.alpha
+    Vbar = engine.Vbar
+    xi = engine.xi
+    dt = expiry / steps
+    xisdt = xi * np.sqrt(dt)
+    erddt = exp((rate - dividend) * dt)
+    egam1 = exp(2*(rate - dividend)*dt)
+    egam2 = -2*erddt+1
+    eveg1 = exp(-alpha*dt)
+    eveg2 = Vbar - Vbar*eveg1
+    
     sumCT = 0
     sumCT2 = 0
-
-    for j in range(M):
-
-        St = S
-        cv = 0
-
-        for i in range(N):
-            t = i * dt
-            delta = BlackScholesDelta(St, t, K, T, sig, r, div)
-            z = np.random.normal(size=1)
-            Stn = St * np.exp(nudt + sigsdt * z)
-            cv = cv + delta * (Stn - St * erddt)
-            St = Stn
-
-        CT = np.maximum(St - K, 0.0) + beta1 * cv
+    
+    for j in range(replications):
+        Vt = volatility * volatility
+        St = spot
+        maxSt = spot
+        cv1 = 0
+        cv2 = 0
+        cv3 = 0
+        
+        s = np.zeros(steps)
+        v = np.zeros(steps)
+        
+        s[0] = spot
+        v[0] = Vbar
+        
+        z1 = np.random.normal(size=steps)
+        z2 = np.random.normal(size=steps)
+        
+        for i in range(steps):
+            t = (i-1) * dt
+            delta = BlackScholesDelta(s[i], t, strike, expiry, volatility, rate, dividend)
+            gamma = BlackScholesGamma(s[i], t, strike, expiry, volatility, rate, dividend)
+            vega = BlackScholesVega(s[i], t, strike, expiry, volatility, rate, dividend)
+            
+            ##### Evolve Variance #####
+            v[i] = v[i-1] + alpha * dt + (Vbar - v[i-1]) + xisdt * z1[i]
+            if v[i] < 0.0: 
+                v[i] = 0.0
+        
+            #####Evolve Asset Price #####
+            s[i] = s[i-1] * np.exp((rate - 0.5 * v[i-1]) * dt + np.sqrt(v[i-1]) * np.sqrt(dt) * z2[i])
+        
+            ####### Accumulate Control Variates ######
+            cv1 = cv1 + delta * (s[i] - s[i-1] * erddt)
+            cv2 = cv2 + gamma * ((s[i] - s[i-1]) * (s[i] - s[i-1]) - s[i-1] * s[i-1] * (egam1 * exp(v[i-1]*dt) + egam2))
+            cv3 = cv3 + vega * ((v[i]- v[i-1])-(Vt * eveg1 + eveg2 - v[i-1]))
+    
+            smax = np.amax(s)
+        
+        CT = np.maximum(smax - strike, 0.0) + beta1 * cv1 + beta2 * cv3 + beta3 * cv3
         sumCT += CT
         sumCT2 += CT * CT
 
-    call_value = (sumCT / M) * np.exp(-r * T)
-    SD = sqrt((sumCT2 - sumCT * sumCT / M) * exp(-2*r*T) / (M-1))
-    return call_value
+        call_value = (sumCT / replications) * np.exp(-rate * expiry)
+        SD = sqrt((sumCT2 - sumCT * sumCT / replications) * exp(-2*rate*expiry) / (replications-1))
+
+        print(call_value, SD)
     
 def Naive_Monte_Carlo_Pricer(engine, option, data):
     expiry = option.expiry
